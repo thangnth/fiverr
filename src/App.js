b4cdca4cc29969f6d73b83d68d555876f6623ecd
thangnth <thangnth0511@gmail.com>
2023-10-27 00:53:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomeLayout from "./layouts/HomeLayout/HomeLayout";
+import AuthLayout from "./layouts/AuthLayout/AuthLayout";
+import UserProfileLayout from "./layouts/UserProfileLayout/UserProfileLayout";
+import ServiceInfo from "./modules/ServiceInfo/ServiceInfo";
+
+import Loading from "./components/Loading/Loading";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+
+const Home = lazy(() => import("./modules/Home/Home"));
+const Login = lazy(() => import("./modules/Auth/Login/Login"));
+const Register = lazy(() => import("./modules/Auth/Register/Register"));
+const UserProfile = lazy(() => import("./modules/UserProfile/UserProfile"));
+const JobList = lazy(() => import("./modules/JobList/JobList"));
+const CategoryDetail = lazy(() =>
+  import("./modules/CategoryDetail/CategoryDetail")
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/jobList/:keyword" element={<JobList />} />
+            <Route
+              path="/:tenLoaiCongViec/:MaLoaiCongViec"
+              element={<CategoryDetail />}
+            />
+            <Route
+              path="/jobList/:keyword/:MaCongViec"
+              element={<ServiceInfo />}
+            />
+          </Route>
+
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+
+          <Route path="/" element={<UserProfileLayout />}>
+            <Route path="/users/:name" element={<UserProfile />} />
+          </Route>
+
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
-
 export default App;
